3fd1c6f3b52884c01c8c570b8c154ba54b3c97c3
Fabian-Burdick <Fabian-Burdick@web.de>
2026-05-27 10:57:14 +0200

    100% Respnsive + Imgs update

--- script.js
+++ script.js
@@ -1,21 +1,40 @@
 
 // Die Bilder
-let myImgs = ["12019-godafoss-1840758.jpg",
-    '12019-iceland-2111810.jpg',
-    '7010naoto-cherryblossoms-9715202.jpg',
-    'anek0o-etretat-10134406.png',
-    'atlantios-boat-3292934.jpg',
-    'ekamelev-insect-3060397.jpg',
-    'jan_meyes-winter-8481557.jpg',
-    'ludo-photos-church-4964183.jpg',
-    'myrichagnon-fire-9550265.jpg',
-    'mystraysoul-iris-5350997.jpg',
-    'naturfreund_pics-iceland-4493216.jpg',
-    'olcayertem-person-6065005.jpg',
-    'paul_reuss-patagonia-10020972.jpg',
-    'ruslansikunov-chamomile-10065194.jpg',
-    'techbro347-rowan-6596302.jpg',
-    'vladgudzovskiy-squirrel-4574759.jpg'];
+let myImgs = [
+    "img01.jpg",
+    'img02.jpg',
+    'img03.jpg',
+    'img04.jpg',
+    'img05.jpg',
+    'img06.jpg',
+    'img07.jpg',
+    'img08.jpg',
+    'img09.jpg',
+    'img10.jpg',
+    'img11.jpg',
+    'img12.jpg',
+    'img13.jpg',
+    'img14.jpg',
+    'img15.jpg',
+    'img16.jpg',
+    'img17.jpg',
+    'img18.jpg',
+    'img19.jpg',
+    'img20.jpg',
+    'img21.jpg',
+    'img22.jpg',
+    'img23.jpg',
+    'img24.jpg',
+    'img25.jpg',
+    'img26.jpg',
+    'img27.jpg',
+    'img28.jpg',
+    'img29.jpg',
+    'img30.jpg',
+    'img31.jpg',
+    'img32.jpg',
+    'img33.jpg'
+];
 
 let currentIndex = 0;
 
@@ -31,8 +50,6 @@ function addImg() {
     }
 }
 
-let dialogRef = document.getElementById("dialog-content")
-
 //Dialog öffnen
 function openDialog(index) {
     currentIndex = index;
@@ -45,12 +62,20 @@ function updateDialog() {
     let ImageSrc = `assets/img/${myImgs[currentIndex]}`
     document.getElementById('dialogImg').src = ImageSrc;
     document.getElementById('numbers').innerText = `${currentIndex + 1} / ${myImgs.length}`; // P tag eingebunden und zählt Zahl im Overlay Hoch und Runter
+    document.getElementById('imgs-numbers').innerText = `IMG ${currentIndex + 1}  `;
 }
 
 //Dialog schließen
 function closeDialog() {
     document.getElementById('dialog-content').style.display = 'none';
 }
+
+// Schließt das Dialog-Element mit der ESC-Taste
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'Escape') {
+        closeDialog();
+    }
+});
 
 // Vorheriges Bild
 function showPrevImg() {
@@ -71,7 +96,6 @@ function showNextImg() {
     }
     updateDialog();
 }
-
 
 window.onload = function () {
     init();
